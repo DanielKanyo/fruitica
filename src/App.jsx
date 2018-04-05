2789c9af95b9e777694c790855cbf6409b15fbea
DanielKanyo <danielkanyo992@gmail.com>
2018-04-05 21:49:06 +0200

    gitignore firebase.js, navigation, home, contact components, react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import * as routes from './constants/routes';
+
+import Home from './components/Home/Home';
+import Contact from './components/Contact/Contact';
+import Navigation from './components/Navigation/Navigation';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Fruitica
-      </div>
+      <Router>
+        <div className="App">
+          <Navigation />
+
+          <Route exact path={routes.HOME} component={() => <Home />} />
+          <Route exact path={routes.CONTACT} component={() => <Contact />} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Home extends React.Component {
+   render() {
+      return (
+         <div>
+            Home fruitica
+         </div>
+      );
+   }
+}
+export default Home;
